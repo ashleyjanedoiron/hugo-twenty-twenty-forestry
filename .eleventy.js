9da2fa9f6d13a4e254ffb945b00cdefe3b5bcc67
Dennis Nieuwenhuis <dennis.nieuwenhuis@iquality.nl>
2021-09-30 08:28:50 +0200

    Add new components

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,26 @@
+// Import filters
+const markdown = require('./src/filters/markdown-filter.js');
+
 module.exports = function (eleventyConfig) {
+
+    /**
+    * Add Filters
+    *
+    * @link https://www.11ty.dev/docs/filters/
+    */
+    eleventyConfig.addFilter('markdown', markdown);
+
+    /**
+    *
+    * Add layout Aliasing
+    *
+    * @link https://www.11ty.dev/docs/layouts/#layout-aliasing
+    */
+    eleventyConfig.addLayoutAlias('home', 'layouts/home.njk');
+    // eleventyConfig.addLayoutAlias('page', 'layouts/single.njk');
+    // eleventyConfig.addLayoutAlias('projects', 'layouts/projects/list.njk');
+    // eleventyConfig.addLayoutAlias('project', 'layouts/projects/single.njk');
+    // eleventyConfig.addLayoutAlias('contact', 'layouts/contact.njk');
 
     // You can return your Config object (optional).
     return {
